43d8407897e2b1c15375943fb25bd60ef5a2173a
Brandon Tat <brandontat7@gmail.com>
2017-04-06 11:33:18 -0700

    added largest_contiguous_subsum_quad

--- execution_time.rb
+++ execution_time.rb
@@ -18,6 +18,16 @@ def my_min_linear(list)
   min
 end
 
-list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-p my_min_quad(list)
-p my_min_linear(list)
+def largest_contiguous_subsum_quad(list)
+  largest_sum = nil
+  (0...list.length).each do |idx|
+    (idx...list.length).each do |idx2|
+      sum = list[idx..idx2].inject(:+)
+      if largest_sum.nil? || largest_sum < sum
+        largest_sum = sum
+      end
+    end
+  end
+  largest_sum
+end
+#p largest_contiguous_subsum_quad([2, 3, -6, 7, -6, 7])
