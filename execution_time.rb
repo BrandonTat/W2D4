4ba496cf2c7caaa8144623f6865a82a7371b71a6
Brandon Tat <brandontat7@gmail.com>
2017-04-06 11:22:04 -0700

    Added my_min_linear

--- execution_time.rb
+++ execution_time.rb
@@ -9,5 +9,15 @@ def my_min_quad(list)
   end
 end
 
+def my_min_linear(list)
+  min = list.first
+  list.each do |el|
+    min = el if el < min
+  end
+
+  min
+end
+
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 p my_min_quad(list)
+p my_min_linear(list)
